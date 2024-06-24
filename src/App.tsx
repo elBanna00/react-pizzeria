1eb52dc61f975cad8329f072806bbfff749cc418
elBanna00 <moaazibrahim3@gmail.com>
2024-06-24 15:01:54 +0300

    feat : Add applayout and rounting
    - Add routing using react router
    - Add file structure
    - Add app root layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,46 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import Home from "./ui/Home";
+import Error from "./ui/Error";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+
+import Order from "./features/order/Order";
+
+import AppLayout from "./ui/AppLayout";
+import CreateOrder from "./features/order/CreateOrder";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    errorElement: <Error />,
+
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+        errorElement: <Error />,
+      },
+      { path: "/cart", element: <Cart /> },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+        errorElement: <Error />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <div>Hello React!</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
