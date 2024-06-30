2332f9a9a724d8d222617e6b848829b74c7009e6
elBanna00 <moaazibrahim3@gmail.com>
2024-06-30 11:39:39 +0300

    feat : Add create user page

--- src/features/user/CreateUser.tsx
+++ src/features/user/CreateUser.tsx
@@ -1,26 +1,70 @@
+// import { FormEvent, useState } from "react";
+
+// function CreateUser() {
+//   const [username, setUsername] = useState("");
+
+//   function handleSubmit(e: FormEvent<HTMLFormElement>) {
+//     e.preventDefault();
+//   }
+
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <p>👋 Welcome! Please start by telling us your name:</p>
+
+//       <input
+//         type="text"
+//         placeholder="Your full name"
+//         value={username}
+//         onChange={(e) => setUsername(e.target.value)}
+//       />
+
+//       {username !== "" && (
+//         <div>
+//           <button>Start ordering</button>
+//         </div>
+//       )}
+//     </form>
+//   );
+// }
+
+// export default CreateUser;
+
 import { FormEvent, useState } from "react";
+import Button from "../../ui/Button";
+import { useDispatch } from "react-redux";
+import { updateName } from "./userSlice";
+import { useNavigate } from "react-router-dom";
 
 function CreateUser() {
   const [username, setUsername] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   function handleSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
+
+    if (!username) return;
+    dispatch(updateName(username));
+    navigate("/menu");
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <p>👋 Welcome! Please start by telling us your name:</p>
+      <p className="mb-4 text-sm text-stone-600 md:text-base">
+        👋 Welcome! Please start by telling us your name:
+      </p>
 
       <input
         type="text"
         placeholder="Your full name"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
+        className="input mb-8 w-72"
       />
 
       {username !== "" && (
         <div>
-          <button>Start ordering</button>
+          <Button type="primary">Start ordering</Button>
         </div>
       )}
     </form>
---
+++ src/features/user/Username.tsx
@@ -0,0 +1,14 @@
+import { useSelector } from "react-redux";
+import { RootState } from "../../store";
+
+function Username() {
+  const username = useSelector((state: RootState) => state.user.username);
+
+  if (!username) return null;
+
+  return (
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  );
+}
+
+export default Username;
