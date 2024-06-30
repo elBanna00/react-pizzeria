5348eddc9800d5f71da701293b33188cdd292e98
elBanna00 <moaazibrahim3@gmail.com>
2024-06-30 11:33:56 +0300

    feat : adjust header
    - Add new logo
    - Add new title

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -2,10 +2,13 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
-      <Link to="/" className="tracking-widest">
-        Fast React Pizza Co.
-      </Link>
+    <header className="flex items-center justify-between border-b border-stone-200 bg-gradient-to-r from-indigo-500 via-purple-500 to-indigo-500 px-4 py-3 uppercase sm:px-6">
+      <div className="flex gap-5">
+        <img src="./public\logo.png" className="h-12 w-12 rounded-full"></img>
+        <Link to="/" className="tracking-widest mt-3">
+          React Pizzeria Co.
+        </Link>
+      </div>
     </header>
   );
 }
