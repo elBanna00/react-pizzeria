6e7207f7568b9ca6b3fcd569525e2db21b0f2348
elBanna00 <moaazibrahim3@gmail.com>
2024-06-30 16:30:04 +0300

    style : update logo path

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -6,7 +6,7 @@ function Header() {
   return (
     <header className="flex items-center justify-between border-b border-stone-200 bg-gradient-to-r from-indigo-500 via-purple-600 to-indigo-500 px-4 py-3 uppercase sm:px-6">
       <div className="flex gap-5">
-        <img src="./public\logo.png" className="h-12 w-12 rounded-full"></img>
+        <img src="/public/logo.png" className="h-12 w-12 rounded-full"></img>
         <Link to="/" className="tracking-widest mt-3">
           React Pizzeria Co.
         </Link>
