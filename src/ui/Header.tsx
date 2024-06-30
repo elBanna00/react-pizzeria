3eedcf0ff8d9f0446e94b987905429e7f83ec509
elBanna00 <moaazibrahim3@gmail.com>
2024-06-30 15:36:27 +0300

    FREE PALASTINE !!

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -1,13 +1,26 @@
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <header className="flex items-center justify-between border-b border-stone-200 bg-gradient-to-r from-indigo-500 via-purple-500 to-indigo-500 px-4 py-3 uppercase sm:px-6">
+    <header className="flex items-center justify-between border-b border-stone-200 bg-gradient-to-r from-indigo-500 via-purple-600 to-indigo-500 px-4 py-3 uppercase sm:px-6">
       <div className="flex gap-5">
         <img src="./public\logo.png" className="h-12 w-12 rounded-full"></img>
         <Link to="/" className="tracking-widest mt-3">
           React Pizzeria Co.
         </Link>
+      </div>
+      <span>
+        <span className=" text-red-600">Fr</span>
+        <span className=" text-black">ee </span>
+        <span className=" text-white">Pala</span>
+        <span className=" text-green-800">stine</span>
+      </span>
+      <div className="flex items-center justify-between gap-5">
+        <SearchOrder />
+
+        <Username />
       </div>
     </header>
   );
